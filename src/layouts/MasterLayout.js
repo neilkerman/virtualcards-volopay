123b8af41e5030ada8031269ce273ec91d663996
neilkerman <contact@samay.dev>
2022-07-10 18:36:52 +0530

    Layout Set
    Added master layout and filter dropdown for 'your' cards

--- src/layouts/MasterLayout.js
+++ src/layouts/MasterLayout.js
@@ -1,8 +1,41 @@
-const MasterLayout = () => {
+import {BiVideo} from "react-icons/bi"
+import {MdAdd} from "react-icons/md"
+import {RiLayoutGridFill} from "react-icons/ri"
+import {GiHamburgerMenu} from "react-icons/gi"
+
+const MasterLayout = ({children}) => {
     return ( 
         <div className="h-screen w-screen overflow-x-hidden">
-            <div className="max-w-6xl w-full mx-auto">
-
+            <div className="max-w-6xl px-5 py-10 w-full mx-auto">
+                <div className="flex flex-col sm:flex-row justify-between">
+                    <div className="flex flex-row items-center">
+                        <h1 className="font-semibold text-3xl text-gray-900">Virtual Cards</h1>
+                        <h6 className="cursor-pointer text-blue-500 text-sm px-3">
+                            <BiVideo className="fill-current text-blue-500 inline-block" size={16} />
+                            <span className="pl-1">Learn More</span>
+                        </h6>
+                    </div>
+                    <button className="shadow-md hover:shadow-lg transition-all duration-300 bg-white px-3 py-1 text-sm flex flex-row items-center rounded"><MdAdd className="fill-current" size={20} /> <span className="pl-2">Virtual Card</span></button>
+                </div>
+                <div className="mt-10 text-xs text-center text-gray-500 border-b border-gray-200 flex flex-row justify-between">
+                    <ul className="flex flex-wrap shrink-0 -mb-px">
+                        <li className="mr-2">
+                            <a className="inline-block px-4 py-3 rounded-t-lg border-b border-transparent hover:text-gray-600 hover:border-gray-300">Your</a>
+                        </li>
+                        <li className="mr-2">
+                            <a className="inline-block px-4 py-3 text-black font-medium rounded-t-lg border-b-2 border-red-600 active">All</a>
+                        </li>
+                        <li className="mr-2">
+                            <a className="inline-block px-4 py-3 rounded-t-lg border-b border-transparent hover:text-gray-600 hover:border-gray-300">Blocked</a>
+                        </li>
+                    </ul>
+                    <div className="flex flex-row shrink-0">
+                        <RiLayoutGridFill className="fill-current text-gray-400 my-auto" size={16} />
+                        <div className="w-3"></div>
+                        <GiHamburgerMenu className="fill-current text-gray-400 my-auto" size={16} />
+                    </div>
+                </div>
+                {children}
             </div>
         </div>
      );
--- src/modules/your-cards.module.js
+++ src/modules/your-cards.module.js
@@ -1,5 +1,71 @@
+import { useState, useRef } from 'react'
+import MasterLayout from "../layouts/MasterLayout";
+import { FiSearch } from "react-icons/fi"
+import { RiFilter3Line } from "react-icons/ri"
+import useClickOutside from '../hooks/useClickOutside';
+
 const YourCards = () => {
-    return ( <></> );
+
+    const [menuOpen, setMenuOpen] = useState(false)
+    const dropdownRef = useRef()
+
+    useClickOutside(dropdownRef, () => {
+        if(menuOpen) setMenuOpen(false)
+    })
+
+    return (
+        <MasterLayout>
+            <div className="w-full flex flex-row items-center justify-end py-5">
+                <FiSearch />
+                <div ref={dropdownRef} className="relative pl-4">
+                    <button onClick={() => { setMenuOpen(prev => !prev) }} className="py-1 px-3 rounded-lg bg-gray-100">
+                        <RiFilter3Line className="fill-current text-gray-400 inline-block" size={18} />
+                        <span className="text-gray-500 text-sm pl-1">Filter</span>
+                    </button>
+                    <div className={`${menuOpen ? '' : 'opacity-0 h-0'} transition-opacity duration-300 overflow-hidden absolute -bottom-30 mt-1 right-0 w-72 rounded shadow-lg border border-gray-100 bg-white`}>
+                        <div className="p-3 border-b border-gray-200">
+                            <h6 className="text-xs font-medium">Filters</h6>
+                        </div>
+                        <div className="p-3">
+                            <h6 className="font-medium text-gray-400 text-xs">
+                                Type
+                            </h6>
+                            <div className="mt-3 flex flex-row">
+                                <div className="flex items-center">
+                                    <input
+                                        id="subcription-checkbox"
+                                        type="checkbox"
+                                        defaultValue=""
+                                        className="w-4 h-4 text-blue-600 rounded-lg focus:ring-0"
+                                    />
+                                    <label
+                                        htmlFor="subcription-checkbox"
+                                        className="ml-2 text-xs text-gray-500 font-medium"
+                                    >
+                                        Subscription
+                                    </label>
+                                </div>
+                                <div className="flex items-center ml-4">
+                                    <input
+                                        id="burner-checkbox"
+                                        type="checkbox"
+                                        defaultValue=""
+                                        className="w-4 h-4 text-blue-600 rounded-lg focus:ring-0"
+                                    />
+                                    <label
+                                        htmlFor="burner-checkbox"
+                                        className="ml-2 text-xs text-gray-500 font-medium"
+                                    >
+                                        Burner
+                                    </label>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </MasterLayout>
+    );
 }
- 
+
 export default YourCards;
